aa4535bdbea6c89b67a297e5a4f6c710c2b70718
Pavel Drabek <pazzicz@gmail.com>
2021-05-23 16:31:41 +0200

    basic form

--- react-app/src/components/DCandidate.js
+++ react-app/src/components/DCandidate.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/dCandidate";
+import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, withStyles } from "@material-ui/core";
+import DCandidateForm from "./DCandidateForm";
 
-const DCandidates = (props) => {
+const styles = theme =>({
+    root: {
+        "& .MuiTableCell-head": {
+            fontSize: "1.25rem"
+        }
+    },
+    paper:{
+        margin: theme.spacing(2),
+        padding: theme.spacing(2)
+    }
+})
+
+
+// props.classes
+// const {classes, ...props} = props
+const DCandidates = ({classes, ...props}) => {
     // const [x, setX] = useState(0)
     // setX(5)
 
@@ -10,7 +27,40 @@ const DCandidates = (props) => {
         props.fetchAllDCandidates()
     }, [])
 
-    return (<div>from DCandidates</div>);
+    return (
+        <Paper className = {classes.paper} elevation={3}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <DCandidateForm />
+                </Grid>
+                <Grid item xs={6}>
+                    <div>list of candidates</div>
+                    <TableContainer>
+                        <Table>
+                            <TableHead className={classes.root}>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Mobile</TableCell>
+                                    <TableCell>Blood Group</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    props.dCandidateList.map((record, index) => {
+                                        return (<TableRow key={index} hover>
+                                            <TableCell>{record.fullName}</TableCell>
+                                            <TableCell>{record.mobile}</TableCell>
+                                            <TableCell>{record.bloodGroup}</TableCell>
+                                        </TableRow>)
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
 }
 
 const mapStateToProps = state => ({
@@ -21,4 +71,4 @@ const mapActionToProps = {
     fetchAllDCandidates: actions.fetchAll
 }
 
-export default connect(mapStateToProps, mapActionToProps)(DCandidates);
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(DCandidates));
---
+++ react-app/src/components/useForm.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from "react";
+
+const useForm = (initialFieldValues) => {
+    const [values, setValues] = useState(initialFieldValues),
+    return ( <div>aa</div> );
+}
+
+export default useForm;
